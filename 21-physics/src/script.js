7420d08bc1810414d99c6121ae5fecb9a66c2620
moon-rhino <107901354+Jo-Moonsoo@users.noreply.github.com>
2023-04-17 23:13:54 -0700

    lesson 21 - physics - apply force

--- 21-physics/src/script.js
+++ 21-physics/src/script.js
@@ -39,17 +39,36 @@ const environmentMapTexture = cubeTextureLoader.load([
 const world = new CANNON.World();
 world.gravity.set(0, -9.82, 0);
 
+// Materials
+const defaultMaterial = new CANNON.Material("concrete");
+const defaultContactMaterial = new CANNON.ContactMaterial(
+  defaultMaterial,
+  defaultMaterial,
+  {
+    friction: 0.1,
+    restitution: 0.7,
+  }
+);
+world.addContactMaterial(defaultContactMaterial);
+world.defaultContactMaterial = defaultContactMaterial;
+
 // Sphere
 const sphereShape = new CANNON.Sphere(0.5);
 const sphereBody = new CANNON.Body({
   mass: 1,
   position: new CANNON.Vec3(0, 3, 0),
   shape: sphereShape,
+  material: defaultMaterial,
 });
+sphereBody.applyLocalForce(
+  new CANNON.Vec3(150, 0, 0),
+  new CANNON.Vec3(0, 0, 0)
+);
 world.addBody(sphereBody);
 
 const floorShape = new CANNON.Plane();
 const floorBody = new CANNON.Body();
+floorBody.material = defaultMaterial;
 floorBody.mass = 0; //object won't move
 floorBody.addShape(floorShape);
 floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI / 2);
@@ -167,6 +186,7 @@ const tick = () => {
   oldElapsedTime = elapsedTime;
 
   // Update physics world
+  sphereBody.applyForce(new CANNON.Vec3(-0.5, 0, 0), sphereBody.position);
   world.step(1 / 60, deltaTime, 3);
 
   sphere.position.copy(sphereBody.position);
