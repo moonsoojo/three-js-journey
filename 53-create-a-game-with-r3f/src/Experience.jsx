543df6eafaf1618998df94b331d223d984087c0b
moon-rhino <107901354+Jo-Moonsoo@users.noreply.github.com>
2023-05-22 07:02:11 -0700

    lesson 53 - create a game

---
+++ 53-create-a-game-with-r3f/src/Experience.jsx
@@ -0,0 +1,16 @@
+import { OrbitControls } from "@react-three/drei";
+import Lights from "./Lights.jsx";
+import Level from "./Level.jsx";
+import { Physics, Debug } from "@react-three/rapier";
+
+export default function Experience() {
+  return (
+    <>
+      <OrbitControls makeDefault />
+      <Physics>
+        <Lights />
+        <Level />
+      </Physics>
+    </>
+  );
+}
---
+++ 53-create-a-game-with-r3f/src/Level.jsx
@@ -0,0 +1,60 @@
+import * as THREE from "three";
+import { RigidBody } from "@react-three/rapier";
+
+THREE.ColorManagement.legacyMode = false; //when you add colors outside of react 3 fiber
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+
+const floor1Material = new THREE.MeshStandardMaterial({ color: "limegreen" });
+const floor2Material = new THREE.MeshStandardMaterial({ color: "greenyellow" });
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: "orangered" });
+const wallMaterial = new THREE.MeshStandardMaterial({ color: "slategrey" });
+
+function BlockStart({ position = [0, 0, 0] }) {
+  return (
+    <group position={position}>
+      {/* floor */}
+      <mesh
+        geometry={boxGeometry}
+        material={floor1Material}
+        position={[0, -0.1, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow
+      />
+    </group>
+  );
+}
+
+function BlockSpinner({ position = [0, 0, 0] }) {
+  return (
+    <group position={position}>
+      {/* floor */}
+      <mesh
+        geometry={boxGeometry}
+        material={floor2Material}
+        position={[0, -0.1, 0]}
+        scale={[4, 0.2, 4]}
+        receiveShadow
+      />
+      <RigidBody>
+        {" "}
+        <mesh
+          geometry={boxGeometry}
+          material={obstacleMaterial}
+          scale={[3.5, 0.3, 0.3]}
+          castShadow
+          receiveShadow
+        />
+      </RigidBody>
+    </group>
+  );
+}
+
+export default function Level() {
+  return (
+    <>
+      <BlockStart position={[0, 0, 4]} />
+      <BlockSpinner position={[0, 0, 0]} />
+    </>
+  );
+}
